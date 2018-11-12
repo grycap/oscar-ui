9fe26b0bb1089379100d550f3335876b1abd580a
dianamnaranjo92 <dianamariand92@gmail.com>
2018-11-12 15:53:12 +0100

    minio url changed

--- node_server/routes/index.js
+++ node_server/routes/index.js
@@ -130,10 +130,8 @@ minioClient = "";
 console.log(minioClient)
 
 var minioClient = new Minio.Client({
-  // endPoint: 'minio-service.minio',  
-  endPoint: '158.42.105.19',  
-  // port: 9000,  
-  port: 31852,  
+  endPoint: 'minio-service.minio',    
+  port: 9000,   
   useSSL: false,
   accessKey: "$MINIO_ACCESS_KEY",
   secretKey: "$MINIO_SECRET_KEY"
