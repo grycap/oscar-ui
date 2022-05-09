7c403216a6e32284a12cc7dd48522bbc428eea04
dianamariand92 <36927150+dianamariand92@users.noreply.github.com>
2022-05-09 10:12:06 +0000

    Deploying to gh-pages from @ grycap/oscar-ui@d33d8cc1f2414dc557adb0f8846f655ebc66b74c 🚀

--- src/env.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    "provider_url": "https://aai-dev.egi.eu",
-    "client_id": "10333f08-7e8f-4cc4-bba0-e66f4e5505d0",
-    "redirect_uri" : "http://localhost:8080/callback.html",
-    "url_authorize": "https://aai-dev.egi.eu/oidc/authorize",
-    "url_user_info": "https://aai-dev.egi.eu/oidc/userinfo",
-    "api" : "http://localhost", // value of the api
-}
