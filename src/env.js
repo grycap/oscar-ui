095b302be2c4135672dbf04e44212664bc648c57
dianamnaranjo92 <dianamariand92@gmail.com>
2022-06-23 11:07:06 +0200

    working on check-in

--- src/env.js
+++ src/env.js
@@ -2,9 +2,9 @@ module.exports = {
     "provider_url": "https://aai-dev.egi.eu/auth/realms/egi/protocol/",
     "client_id": "417e2915-5e6f-4f50-b4e8-59ffd938957d",
     "redirect_uri" : `${location.origin}/callback.html`,
-    "url_authorize": `${provider_url}openid-connect/auth`,
-    "url_user_info": `${provider_url}openid-connect/userinfo`,
-    "token_endpoint": `${provider_url}openid-connect/token`,
+    "url_authorize": "https://aai-dev.egi.eu/auth/realms/egi/protocol/openid-connect/auth",
+    "url_user_info": "https://aai-dev.egi.eu/auth/realms/egi/protocol/openid-connect/userinfo",
+    "token_endpoint": "https://aai-dev.egi.eu/auth/realms/egi/protocol/openid-connect/token",
     "api" : "", // value of the api,
     "deploy_container": "false",
 }
--- src/main.js
+++ src/main.js
@@ -13,6 +13,24 @@ Vue.config.productionTip = false
 window.axios = require('axios');
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
+window.axios.interceptors.request.use(function (config) {
+  // Do something before request is sent
+  // const AUTH_TOKEN = document.getElementsByName("token")["0"].content;
+  var session = JSON.parse(localStorage.getItem("session"))
+  const AUTH_TOKEN = session.user.access_token
+
+  if(AUTH_TOKEN){
+      config.headers.common['Authorization'] = `Bearer ${AUTH_TOKEN}`
+  }
+  return config;
+}, function (error) {
+  // Do something with request error
+  console.log(error);
+  this.$router.replace(this.$route.query.redirect || "/logout");
+  return Promise.reject(error.response);
+});
+
+
 Vue.mixin({
   data: function(){
     return {
