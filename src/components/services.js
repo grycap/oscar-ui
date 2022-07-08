80ffe739497d9efd25fc7099747641e14c0ca955
SergioLangaritaBenitez <sergiolangaritabenitez@gmail.com>
2022-07-08 13:51:01 +0200

    ssl problem

--- src/components/services.js
+++ src/components/services.js
@@ -12,10 +12,10 @@ export default {
     },
     created(){
         this.username_auth = localStorage.getItem("user");
-        this.password_auth = localStorage.getItem("password");        
+        this.password_auth = localStorage.getItem("password");
         var minio_endpoint = localStorage.getItem("endpoint");
         var minio_port = localStorage.getItem("port");
-        var minio_useSSL = localStorage.getItem("useSSL")?localStorage.getItem("useSSL"):true;
+        var minio_useSSL = localStorage.getItem("useSSL");
         var minio_accessKey = localStorage.getItem("accessKey");
         var minio_secretKey = localStorage.getItem("secretKey");
         this.api = localStorage.getItem('api');
